3e29bf9cb712e7d1eee8d5a0b1727dd56a755ce7
Daniel Bandala <daniel_4515@hotmail.com>
2025-06-04 12:57:26 -0600

    pdf knowledge retrieval

--- pdf_retrieval/pdf_rag.py
+++ pdf_retrieval/pdf_rag.py
@@ -1,43 +1,55 @@
 import os
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+from pydantic import SecretStr
 
-from langchain.chat_models import ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+
+from langchain_text_splitters.character import RecursiveCharacterTextSplitter
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
+
+
+
+# from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 
+load_dotenv()
 
-
-# Ensure you have the necessary libraries installed:
+# Load the PDF document
 loader = PyPDFLoader("../Tax-interview.pdf")
-documents = loader.load()
+document = loader.load()
 
+# Split the document into smaller chunks for better processing
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-docs = text_splitter.split_documents(documents)
+docs = text_splitter.split_documents(document)
 
-
-
+# build the vectorstore using OpenAI embeddings
 embeddings = OpenAIEmbeddings(
-    model="text-embedding-ada-002",
-    openai_api_key=os.getenv("OPENAI_API_KEY")  # Ensure you have set your OpenAI API key in the environment variables
+    model="text-embedding-3-small",
+    api_key=SecretStr(os.getenv("OPENAI_API_KEY") or ""),  # Ensure your API key is set in the environment
 )
 vectorstore = FAISS.from_documents(docs, embeddings)
 
 
+print(f"Number of documents in the vectorstore: {vectorstore.index.ntotal}\n\n")
 
 
+# Create a retriever and LLM for question answering
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+
 llm = ChatOpenAI(
-    model_name="gpt-3.5-turbo",
-    openai_api_key=os.getenv("OPENAI_API_KEY")  # Ensure your API key is set in the environment
+    model="gpt-4o-mini",
+    api_key=SecretStr(os.getenv("OPENAI_API_KEY") or ""),  # Ensure your API key is set in the environment,
+    temperature=0.0  # Set temperature to 0 for deterministic responses
 )
 
-qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+qa_chain = RetrievalQA.from_llm(llm=llm, retriever=retriever)
 
 
 query = "What is the main topic of the document?"
-response = qa_chain.run(query)
-print(response)
+response = qa_chain.invoke(query)
+print(response, "\n\n")
 
